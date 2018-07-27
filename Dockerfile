d09cc8ebd899777e5b5e16d588b5c275619f466a
oriolrt <oriolrt@cvc.uab.es>
2018-07-27 09:38:41 +0200

    modificat:        Dockerfile 	modificat:        createUser.sh

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,14 @@ FROM mongo
 MAINTAINER Oriol Ramos Terrades <oriol.ramos@uab.cat>
 
 RUN apt-get update && \
-	apt-get install -y openssh-client \
+	apt-get install -y apg \
+  openssh-client \
 	openssh-server \
   vim
 
 COPY createUser.sh /root/createUser.sh
-#RUN chmod +x /root/createUser.sh
-#RUN /root/createUser.sh
-#RUN rm /root/createUser.sh
+RUN chmod +x /root/createUser.sh
+RUN /root/createUser.sh && rm /root/createUser.sh
 
 EXPOSE 27017
 EXPOSE 22
--- createUser.sh
+++ createUser.sh
@@ -5,17 +5,14 @@ username=student
 password=student
 
 if [ $(id -u) -eq 0 ]; then
-#  read -p "Enter username : " username
-#  read -s -p "Enter password : " password
   egrep "^$username" /etc/passwd >/dev/null
-  pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
 
   if [ $? -eq 0 ]; then
-	echo "$username exists! changing password."
-	echo "${username}:${pass}" | passwd &> /dev/null
-	exit 0
+	  echo "$username exists! changing password."
+	  echo "${username}:${password}" | passwd &> /dev/null
+	  exit 0
   else
-	useradd -m -p $pass $username -s /bin/bash
+	useradd -m -p ${password} $username -s /bin/bash
 
 	[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
  fi
